8de2be24b33a9e2e2c57fe9e0daa67be3838dc88
Your Name <you@example.com>
2017-03-02 17:53:04 +0800

    uwsgi+nginx

--- CMDB/CMDB/settings.py
+++ CMDB/CMDB/settings.py
@@ -51,22 +51,22 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/web/CMDB/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/web/CMDB/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/web/CMDB/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/web/CMDB/app/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
